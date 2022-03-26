693b23e476d8e4f22b3098cbd4023f380288e1c6
Volosenco Andreea-Laura <91089388+imj31us4am50@users.noreply.github.com>
2022-03-26 20:03:05 +0200

    upd

--- footer/Footer.js
+++ footer/Footer.js
@@ -5,7 +5,7 @@ export default function Footer() {
     <div className="container-fluid footer text-white text-center p-2 pt-4">
       <div className="row h5 opacity-50 mx-auto">
         <div class="col-12 col-lg-6 col-md-12">
-          <p className="fs ojd">
+          <p className="fs ojd mx-auto">
             „Omenirea trebuie să învețe să înțeleagă că viața unui animal nu
             este în niciun fel mai puțin prețioasă decât a noastră.”
           </p>
