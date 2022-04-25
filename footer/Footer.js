f6305c897589e8178d278c519c0ab990237ee064
Volosenco Andreea-Laura <91089388+imj31us4am50@users.noreply.github.com>
2022-04-25 11:06:27 +0300

    upd

--- footer/Footer.js
+++ footer/Footer.js
@@ -2,7 +2,10 @@ import FooterCSS from "/footer/Footer.css";
 
 export default function Footer() {
   return (
-    <div className="container-fluid footer text-white text-center pt-3 pb-3">
+    <div
+      className="container-fluid footer text-white text-center pt-3 pb-3"
+      style={FooterCSS}
+    >
       <div className="row h5 opacity-50 mx-auto">
         <div class="col-4 text-center awa">Ajutăm.</div>
         <div class="col-4 text-center awa">Protejăm.</div>
